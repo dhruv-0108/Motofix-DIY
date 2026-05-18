c18dfdea45c350d152f14ee4ec49a9efda5230db
Dhruv Varachhiya <dhruvofficial0108@gmail.com>
2026-05-18 18:50:05 +0530

    Update models with data from new manuals

--- data/royal-enfield/models.ts
+++ data/royal-enfield/models.ts
@@ -130,5 +130,111 @@ export const royalEnfieldModels: BikeModel[] = [
       },
       battery: "12V, 8 Ah, VRLA"
     }
+  },
+  {
+    id: "continental-gt-650",
+    name: "Continental GT 650",
+    brand: "Royal Enfield",
+    years: [2019, 2020, 2021, 2022, 2023, 2024],
+    specs: {
+      engineCc: "648cc",
+      engineType: "Inline twin cylinder, 4 stroke / SOHC",
+      fuelType: "Petrol",
+      fuelTankCapacity: "12.5 Litres",
+      drive: "Chain Drive",
+      engineOilGrade: "10W-50 API SL (or higher) JASO MA2 Fully Synthetic",
+      oilCapacity: "3.1 Litres",
+      tyres: { front: "100/90-18", rear: "130/70-18" },
+      tyrePressure: {
+        city: { front: "32 psi", rear: "36 psi" },
+        highway: { front: "32 psi", rear: "39 psi" }
+      },
+      battery: "12V, 12 Ah, VRLA"
+    }
+  },
+  {
+    id: "interceptor-650",
+    name: "Interceptor 650",
+    brand: "Royal Enfield",
+    years: [2019, 2020, 2021, 2022, 2023, 2024],
+    specs: {
+      engineCc: "648cc",
+      engineType: "Inline twin cylinder, 4 stroke / SOHC",
+      fuelType: "Petrol",
+      fuelTankCapacity: "13.7 Litres",
+      drive: "Chain Drive",
+      engineOilGrade: "10W-50 API SL (or higher) JASO MA2 Fully Synthetic",
+      oilCapacity: "3.1 Litres",
+      tyres: { front: "100/90-18", rear: "130/70-18" },
+      tyrePressure: {
+        city: { front: "32 psi", rear: "36 psi" },
+        highway: { front: "32 psi", rear: "39 psi" }
+      },
+      battery: "12V, 12 Ah, VRLA"
+    }
+  },
+  {
+    id: "super-meteor-650",
+    name: "Super Meteor 650",
+    brand: "Royal Enfield",
+    years: [2023, 2024],
+    specs: {
+      engineCc: "648cc",
+      engineType: "Inline twin cylinder, 4 stroke / SOHC",
+      fuelType: "Petrol",
+      fuelTankCapacity: "15.7 Litres",
+      drive: "Chain Drive",
+      engineOilGrade: "10W-50 API SL (or higher) JASO MA2 Fully Synthetic",
+      oilCapacity: "3.1 Litres",
+      tyres: { front: "100/90-19", rear: "150/80-16" },
+      tyrePressure: {
+        city: { front: "32 psi", rear: "36 psi" },
+        highway: { front: "32 psi", rear: "42 psi" }
+      },
+      battery: "12V, 12 Ah, VRLA"
+    }
+  },
+  {
+    id: "shotgun-650",
+    name: "Shotgun 650",
+    brand: "Royal Enfield",
+    years: [2024],
+    specs: {
+      engineCc: "648cc",
+      engineType: "Inline twin cylinder, 4 stroke / SOHC",
+      fuelType: "Petrol",
+      fuelTankCapacity: "13.8 Litres",
+      drive: "Chain Drive",
+      engineOilGrade: "10W-50 API SL (or higher) JASO MA2 Fully Synthetic",
+      oilCapacity: "3.1 Litres",
+      tyres: { front: "100/90-18", rear: "150/70-17" },
+      tyrePressure: {
+        city: { front: "32 psi", rear: "36 psi" },
+        highway: { front: "32 psi", rear: "42 psi" }
+      },
+      battery: "12V, 12 Ah, VRLA"
+    }
+  },
+  {
+    id: "guerrilla-450",
+    name: "Guerrilla 450",
+    brand: "Royal Enfield",
+    years: [2024],
+    specs: {
+      engineCc: "452cc",
+      engineType: "Liquid Cooled, Single Cylinder, DOHC, 4 Valves",
+      fuelType: "Petrol",
+      fuelTankCapacity: "11 Litres",
+      drive: "Chain Drive",
+      engineOilGrade: "10W40 API SN, JASO MA2 Fully Synthetic",
+      oilCapacity: "2.1 Litres",
+      coolantType: "Ethylene Glycol 50:50",
+      tyres: { front: "120/70-17", rear: "160/60-17" },
+      tyrePressure: {
+        city: { front: "32 psi", rear: "32 psi (solo) / 36 psi (pillion)" },
+        highway: { front: "32 psi", rear: "36 psi" }
+      },
+      battery: "12V, 8 Ah, VRLA"
+    }
   }
 ];
